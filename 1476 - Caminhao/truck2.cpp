df94d48c7c53ca8de11a3f23989ee90120c5a71a
Mlord <x_fire_alchemist_x@hotmail.com>
2014-04-11 20:42:30 -0300

    inicialização do LCA indo bem, aparentemente

--- 1476 - Caminhao/truck2.cpp
+++ 1476 - Caminhao/truck2.cpp
@@ -3,21 +3,19 @@
 #include <cstdlib>
 #include <cmath>
 #include <list>
+#include <cstdio>
 
 using namespace std;
 
 #define MAXV 20000
 #define LOG(x) ceil(log2(x))
 
-
-
-
 int id[MAXV], tam[MAXV];
 
-int u[MAXV], v[MAXV], peso[MAXV],pai[MAXV][LOG(MAXV)],nivel[MAXV],checado[MAXV];
-vector<int> x, y, custo;
+int u[MAXV], v[MAXV], peso[MAXV],pai[MAXV][MAXV],nivel[MAXV],checado[MAXV];
+int x[MAXV], y[MAXV], custo[MAXV];
 
-list<int> adj[];
+list<int> adj[MAXV];
 list<int>::iterator it;
 
 
@@ -51,38 +49,42 @@ int tradutor[MAXV];
 
 /******************************************************************************************/
 /***************Inicialização da LCA*******************************************************/
-void dfsR(int n,int v){
-  checado[v] == 1;
-
+void dfsR(int n,int V,int Ant){
+  checado[V] = 1;
   for(int i = 0; i < n; i++)
-    for(it = adj[tradutor[v]].begin(); it!= adj[tradutor[v]].end(); it++){
-      if(checado[*it] == -1){ dfsR(n,*it); }
+    //    for(it = adj[V].begin(); it != adj[V].end(); it++){
+    while(!adj[V].empty()){
+      it = adj[V].begin();
+      if(checado[*it] == -1){ dfsR(n,*it,V); }
       else {
-	//	pai[v][0] = pai dele na arvore;
+	pai[V][0] = Ant; //o pai de alguem é o anterior à ele, sendo o anterior da raiz ela mesma
+	if(V!= Ant)nivel[V] = nivel[Ant] + 1; // o nivel de alguem é o nivel do seu pai + 1
 	for(int k = 1; k < LOG(n); k++)
-	  pai[v][k] = pai[pai[v][k-1]][k-1];
+	  pai[V][k] = pai[pai[V][k-1]][k-1];
+	adj[V].pop_front();
       }
     } 
   
 }
 
 void LCAinit(int n){
-  checado[tradutor[0]] == 1;
-  for(int i = 0; i < LOG(n); i++){ pai[tradutor[0]][i] = 0; /*pai[i][0] = pai de fato dele*/} 
-  for(int i = 1; i < n; i++) checado[tradutor[i]] = -1;
-  dfsR(n,tradutor[1]);
+  for(int i = 0; i < n; i++) checado[i] = -1;
+  for(int i = 0; i < n; i++) nivel[i] = 0;
+  nivel[0] = 1;
+  dfsR(n,0,0);
 }
 /******************************************************************************************/
 /******************************************************************************************/
 
 int LCA(int n,int a, int b){
   int tmp;
-  if(nivel[a] == nivel[b]) return pai[a][0];
-  if(nivel[a] < nivel[b])
+  int A = tradutor[a];
+  int B = tradutor[b];
+  //  if(nivel[a] < nivel[b])
 }
 
-
-
+/******************************************************************************************/
+/******************************************************************************************/
 
 int compare(const void * a, const void * b)
 {
@@ -95,29 +97,39 @@ int compare(const void * a, const void * b)
 				       
 
 int main(){
-  tam = 0;
-  while(cin >> N >> M >> S){
 
-    for(int i = 0; i < M; i++){ cin >> u[i] >> v[i] >> peso[i];u[i]--; v[i]--; tradutor[i] = i;  }
+  while(cin >> N >> M >> S){
+    for(int i = 0; i < M; i++){ cin >> u[i] >> v[i] >> peso[i];u[i]--; v[i]--; tradutor[i] = i;}
 
     qsort(tradutor,M,sizeof(int),compare);   //ordena decrescentemente as arestas
    
     UFInit(N);
 
     for(int j = 0; j < M; j++){ // for que forma a árvore geradora de custo máximo
-      if(UFFind(u[tradutor[j]]) != UFFind(v[tradutor[j]])){ 
-	UFUnion(u[tradutor[j]],v[tradutor[j]]); 
-      
-	adj[u[tradutor[j]]].push_back(v[tradutor[j]]); // u é adjacente a v
-	adj[v[tradutor[j]]].push_back(u[tradutor[j]]); // v é adjacente a u
-      
-	x[j] = u[j]; 
-	y[j] = v[j];  
-	custo[j] = peso[j];
+      int U = u[tradutor[j]];
+      int V = v[tradutor[j]];
+      if(UFFind(U) != UFFind(V)){ 
+
+	UFUnion(U,V); 
+
+	cout << "adicionei a aresta " << U+1 << "-" << V+1 << " custo: " << peso[tradutor[j]] << endl;
+	x[tradutor[j]] = U; 
+	y[tradutor[j]] = V;
+	custo[tradutor[j]] = peso[tradutor[j]];
+
+
+
+	adj[U].push_back(V); // u é adjacente a v
+	adj[V].push_back(U); // v é adjacente a u
       }
     }
-  
-  
+
+    LCAinit(N);
+
+    for(int i = 0; i < N; i++)
+      for(int j = 0; j < LOG(N); j++)
+	cout <<"i= " << i << " j=" << j << " pai=" <<  pai[i][j] << "nivel=" << nivel[i] << endl;
+
     for(int i = 0; i < S; i++){
       cin >> ini >> fim;
       ini--;
@@ -128,3 +140,5 @@ int main(){
 
   return 0;
 }
+  
+
