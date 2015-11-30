caecdcd78bca1f4027cf605de0ebceb56f1770d2
Matt Barros <matbarrosrodrigues@gmail.com>
2015-11-30 15:33:17 -0200

    1 feito :)

---
+++ Desafios/Matt/aval3/Codeforces/284a/primroots.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int p;
+
+int main(){
+  cin >> p;
+
+  int resp = 0;
+  for(int i = 1; i < p; i++){
+    int count = 0; 
+    int x = i;
+    if((x-1)%p == 0) count--;
+    for(int j = 2; j < p; j++){
+      x = (x*i)%p;
+      if((x-1)%p == 0){
+	if(j == p-1) count++;
+	else count--;
+      }
+    }
+    if(count == 1) resp++;
+  }
+  if(p == 2){ cout << 1 << endl; return 0; }
+  cout << resp << endl;
+  
+  return 0;
+}
