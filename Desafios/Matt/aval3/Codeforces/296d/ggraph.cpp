3aa06908456b7b30475639176555898ca233b451
Mlordx <matbarrosrodrigues@gmail.com>
2015-12-05 16:56:24 -0200

    Feito o Floyd-Warshall com remoções!

--- Desafios/Matt/aval3/Codeforces/296d/ggraph.cpp
+++ Desafios/Matt/aval3/Codeforces/296d/ggraph.cpp
@@ -2,48 +2,38 @@
 
 using namespace std;
 
-#define INF 1 << 30
-
 int N;
 
 int adj[501][501];
 
-int fw(int n){
+vector<int> deleted(501);
+vector<bool> valid(501);
+vector<long long> answer(501);
+
+void fw(){
   
   int min_dist[501][501];
 
-
   for(int i = 1; i <= N; i++)
-    for(int j = 1; j <= N; j++)
-      min_dist[i][j] = INF;
-
-    for(int i = 1; i <= N; i++)
     for(int j = 1; j <= N; j++)
       min_dist[i][j] = adj[i][j];
 
 
-  for(int k = 1; k < N; k++){
+  for(int k = 1; k <= N; k++){
+    answer[k] = 0;
+    int aux = deleted[k];
+    valid[aux] = true;
+
     for(int i = 1; i <= N; i++){
       for(int j = 1; j <= N; j++){
-	if(min_dist[i][j] > min_dist[i][k] + min_dist[k][j])
-	  min_dist[i][j] = min_dist[i][k] + min_dist[k][j];
+	min_dist[i][j] = min(min_dist[i][j],min_dist[i][aux] + min_dist[aux][j]);
+	if(valid[i] && valid[j]) answer[k] += min_dist[i][j];
       }
     }
   }
-
-  int sum = 0;
-  for(int i = 1; i <= N; i++)
-    for(int j = 1; j <= N; j++)
-      if(min_dist[i][j] != INF)
-	sum += min_dist[i][j];
   
-  for(int i = 1; i <= N; i++){
-    adj[n][i] = 0;
-    adj[i][n] = 0;
-  }
+ 
   
-
-  return sum;
 }
 
 int main(){
@@ -56,14 +46,17 @@ int main(){
 
   int aux;
 
-  for(int i = 1; i < N; i++){
-    cin >> aux;
-    cout << fw(aux) << " ";
+  for(int i = N; i >= 1; i--){
+    cin >> deleted[i];
+    valid[i] = false;
   }
 
-  cin >> aux;
-  cout << fw(aux) << endl;
+  fw();
 
-
+  for(int i = N; i>=1; i--){
+    cout << answer[i] << " ";
+  }
+  cout << endl;
+  
   return 0;
 }
