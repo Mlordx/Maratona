df471c1d0a93aeb6b1883994a5d5ed56f0e3acc3
Mlordx <matbarrosrodrigues@gmail.com>
2015-12-08 19:36:14 -0200

    É TETRAAAAAAAA

--- Desafios/Matt/aval3/Timus/1207/plane.cpp
+++ Desafios/Matt/aval3/Timus/1207/plane.cpp
@@ -1,66 +1,47 @@
+/*
+Nome: Mateus Barros Rodrigues
+NUSP: 7991037
+MAC0327 - 2o Sem 2015
+3a Aval
+Problema P17 - 1207.Median on the plane
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int X[10001];
-int Y[10001];
-
-int indMinY = -1;
-int minY = 1 << 30;
-
-inline int left(int i, int j, int k){
-  return (X[j] - X[i])*(Y[k] - Y[i]) - (X[k] - X[i])*(Y[j] - Y[i]);
-}
-
-
-vector<int> translate(10001);
-
-inline int isLeft(int i, int j, int k){ return left(i,j,k) < 0; }
-inline int isRight(int i, int j, int k){ return left(i,j,k) > 0; }
-
-bool cmp(const int i,const int j){
-  return left(indMinY,j,i);
-}
-
 
 int N;
 
+struct Point{
+  long long first;
+  long long second;
+  int pos;
+};
+
+vector<Point> points;
+
+
+long long left(Point A, Point B, Point C){
+  return (B.first - A.first)*(C.second - A.second)-(B.second - A.second)*(C.first - A.first);
+}
+
+bool cmp(const Point& A, const Point& B){
+  return left(points[0],A,B) < 0;
+}
+
 int main(){
   cin >> N;
+  points.resize(N);
 
   for(int i = 0; i < N; i++){
-    translate[i] = i;
-    cin >> X[i] >> Y[i];
-    if(Y[i] < minY){
-      minY = Y[i];
-      indMinY = i;
-    }
+    cin >> points[i].first >> points[i].second;
+    points[i].pos = i;
   }
-
-
-  swap(translate[0],translate[indMinY]);
-  //  swap(X[0],X[indMinY]);
-  //swap(Y[0],Y[indMinY]);
-
-  sort(translate.begin()+1,translate.end(),cmp);
-
-  for(int i = 0; i < N; i++) cout << translate[i] << " ";
-  cout << endl << endl;
   
-  vector<int> H;
+  sort(points.begin()+1,points.end(),cmp);
 
-  H.push_back(0);
-  H.push_back(1);
-  H.push_back(2);
-
-  int h = 2;
-
-  for(int k = 3; k < N; k++){
-    while(isLeft(H[h],H[h-1],k)) h--;
-    H[++h] = k;
-  }
-
-  for(int i = 0; i <= h;i++) cout << X[H[i]] << "~" << Y[H[i]] << endl;
+  cout << "1 " << points[N/2].pos+1 << endl;
   
   return 0;
 }
